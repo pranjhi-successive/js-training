14b01e0b64318eb6c621eacd2231d70f9339047e
pranjhi-successive <pranjhi.singh@successive.tech>
2023-10-06 11:26:21 +0530

    updated changes

--- Assignment_Day3/Question6.js
+++ Assignment_Day3/Question6.js
@@ -1,15 +1,16 @@
 //Write a program to display prime numbers from 1 to 50
 console.log("Display Prime Number From 1 to 50");
 function primeNumber(num){
-   if(num<=1)return false;
-   if(num<=3)return true;
-   for(let i=2;i*i<=num;i++){
-    if(num%i==0){
-        return false;
+    if(num<=1)return false;
+    if(num<=3)return true;
+    
+    for(let i=2; i<num; i++){
+     if(num%i==0){
+         return false;
+     }
     }
-   }
-   return true;
-}
+    return true;
+ }
 for(let num=1;num<=50;num++){
     if(primeNumber(num)){
         console.log(num);
